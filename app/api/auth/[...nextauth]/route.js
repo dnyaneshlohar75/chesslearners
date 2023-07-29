90ded020cf4b7afa86a9dc9deb69a57abe3d33d1
dnyanesh75 <dnyaneshlohar75@gmail.com>
2023-07-29 19:36:25 +0530

    Auth Options

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import { NextResponse } from "next/server";
 
-export const authOptions = NextAuth({
+const authOptions = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
